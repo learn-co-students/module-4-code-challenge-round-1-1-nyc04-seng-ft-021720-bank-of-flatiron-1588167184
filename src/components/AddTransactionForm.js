ff2eea0bea4d3c66c930084ed3ca75b7e21849a4
erocknine <echeungsk@gmail.com>
2020-04-29 11:04:00 -0400

    main deliverables done

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,18 +1,68 @@
 import React, { Component } from "react";
 
+const formatDate = (date) => {
+  let d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+  if (month.length < 2) 
+      month = '0' + month;
+  if (day.length < 2) 
+      day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+
+const initialState = {
+  date: formatDate(new Date()),
+  description: "",
+  category: "",
+  amount: ""
+}
+
 class AddTransactionForm extends Component {
+
+  state = initialState
+
+  handleInput = (event) => {
+    const input = event.target.name
+    this.setState({
+      [input]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    fetch('http://localhost:6001/transactions', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.state)
+    })
+      .then(response => response.json())
+      .then(transaction => {
+        this.props.addTransaction(transaction)
+        this.setState(initialState)
+      })
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.handleSubmit}>
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" value={this.state.date} onChange={this.handleInput}/>
+            <input type="text" name="description" placeholder="Description" value={this.state.description} onChange={this.handleInput}/>
+            <input type="text" name="category" placeholder="Category" value={this.state.category} onChange={this.handleInput}/>
             <input
               type="number"
               name="amount"
               placeholder="Amount"
+              value={this.state.amount}
+              onChange={this.handleInput}
               step="0.01"
             />
           </div>
@@ -26,3 +76,5 @@ class AddTransactionForm extends Component {
 }
 
 export default AddTransactionForm;
+
+
